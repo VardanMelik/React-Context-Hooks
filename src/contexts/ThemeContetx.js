d5bb8a2bc5df7848dda7666f189ff1922ca7033b
EYE MOTION <eyemotiongamestudio@gmail.com>
2019-11-07 15:10:19 +0400

    update

--- src/contexts/ThemeContetx.js
+++ src/contexts/ThemeContetx.js
@@ -8,9 +8,13 @@ export default class ThemeContextProvider extends Component {
         light: { syntax: '#555', ui: '#ddd', bg: '#eee' },
         dark:  { syntax: '#ddd', ui: '#333', bg: '#555' }
     }
+    toggleTheme = () => {
+        console.log(this.state.isLightTheme)
+        this.setState({ isLightTheme: !this.state.isLightTheme})
+    }
     render () {
         return ( 
-            <ThemeContext.Provider value={{...this.state}}>
+            <ThemeContext.Provider value={{...this.state, toggleTheme: this.toggleTheme}}>
                 {this.props.children}
             </ThemeContext.Provider>
         )
